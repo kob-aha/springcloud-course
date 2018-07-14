1ea464cdc483df951d1bc9598c8c1443a7d88dd7
Koby Aharon <kobyahron@gmail.com>
2018-07-14 19:12:53 +0300

    Add client project and aggregator pom

---
+++ lab-3/client/src/main/java/edu/ka/springcloud/lab3client/Lab3ClientApplication.java
@@ -0,0 +1,13 @@
+package edu.ka.springcloud.lab3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Lab3ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ClientApplication.class, args);
+	}
+}
