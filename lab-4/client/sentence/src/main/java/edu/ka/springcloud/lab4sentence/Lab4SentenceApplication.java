2e67073d4241d8e101a98c985c4ffa47ed7fac28
Koby Aharon <kobyahron@gmail.com>
2018-10-26 19:30:12 +0300

    Add controller class the assemble and return a sentence

--- lab-4/client/sentence/src/main/java/edu/ka/springcloud/lab4sentence/Lab4SentenceApplication.java
+++ lab-4/client/sentence/src/main/java/edu/ka/springcloud/lab4sentence/Lab4SentenceApplication.java
@@ -2,7 +2,9 @@ package edu.ka.springcloud.lab4sentence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Lab4SentenceApplication {
 
