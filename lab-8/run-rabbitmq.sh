882bcaeac3196fc2c1e89c01fe6968cc19d77785
Koby Aharon <kobyahron@gmail.com>
2019-01-27 21:22:36 +0200

    Add lab-8 solution

--- lab-8/run-rabbitmq.sh
+++ lab-8/run-rabbitmq.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --hostname my-rabbit --rm --name some-rabbit rabbitmq:3
+#docker run -p 4369:4369/tcp \
+#	-p 5671:5671/tcp \
+#	-p 5672:5672/tcp \
+#	-p 25672:25672/tcp \
+#	-p 15672:15672 \
+#	-d --rm --name some-rabbit rabbitmq:3
+
+docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
